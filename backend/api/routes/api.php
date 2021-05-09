00d2962eda72c462dad065849913a3b232ed3cb2
Ángel Quiroz <guirudj007@gmail.com>
2021-05-09 16:09:23 -0500

    Users

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -49,7 +49,9 @@ Route::group([
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('test', [AuthController::class, 'test'])->name('test');
+
     Route::apiResources([
+        'drivers' => DriverController::class,
         'users' => UserController::class,
         'markers' => MarkerController::class,
     ]);
